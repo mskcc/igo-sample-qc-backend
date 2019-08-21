c2535a70d5921003df42fb20dbdad5da69177189
Lisa Wagner <wagnerl@mskcc.org>
2019-08-21 13:35:04 -0400

    dump comments as json

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # app/__init__.py
 
 from flask import Flask
+import json
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -21,22 +22,20 @@ db.create_all()
 # def index():
 #     return "Welcome to Sample QC Reporting"
 
+
 @app.route("/")
 def comment():
-	comments = Comment.query.all()
+    comments = Comment.query.all()
+    comments_response = []
+    for comment in comments:
+        comments_response.append(comment.serialize)
+        columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
+    return json.dumps(comments_response)
 
-	comments_response = []
-	for comment in comments:
-		comments_response.append(comment.serialize)
-		columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
-	return comments_response
+    # submissions = Submission.query.filter(Submission.username == username).all()
 
-
-	# submissions = Submission.query.filter(Submission.username == username).all()
-
-	# submissions_response = []
-	# for submission in submissions:
-	    # submissions_response.append(submission.serialize)
-	#     # columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
-	# return submissions_response
-
+    # submissions_response = []
+    # for submission in submissions:
+    # submissions_response.append(submission.serialize)
+    #     # columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
+    # return submissions_response
