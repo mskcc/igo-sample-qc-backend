92ce98319f98e1be1036994f2390462646fedd26
anna patruno <apatruno618@gmail.com>
2019-08-22 16:19:21 -0400

    addComment route adds to db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # app/__init__.py
 
 from flask import Flask
+from datetime import datetime
 import json
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -35,6 +36,41 @@ def comment():
     return json.dumps(comments_response)
 
 
+@app.route("/addComment")
+def save_comment():
+    # some json
+    payload = '{            "username": "duniganm",            "user_title": "project manager",            "comment": "qc is ready",            "request_id": "06304_B",            "qc_table": "dna"      }'
+
+    # parse payload
+    pycomment = json.loads(payload)
+
+    # python dictionary is saved to db
+    comment = Comment(
+        username=pycomment["username"],
+        user_title=pycomment["user_title"],
+        comment=pycomment["comment"],
+        request_id=pycomment["request_id"],
+        qc_table=pycomment["qc_table"],
+        date_created=datetime.now(),
+        date_updated=datetime.now(),
+    )
+    db.session.add(comment)
+    db.session.commit()
+
+    comments = Comment.query.all()
+    comments_response = []
+    for x in comments:
+        comments_response.append(x.serialize)
+        # columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
+
+    return json.dumps(comments_response)
+    responseObject = {
+        'comments': comments,
+    }
+
+    return make_response(jsonify(responseObject), 200, None)
+
+
 # /addComment
 # https://www.w3schools.com/python/python_json.asp
 # accepts a new comment like
@@ -48,24 +84,22 @@ def comment():
 # json - > python dictionary
 #     payload = {"username": "patrunoa", "user_title": "app analyst", "comment": "qc is ready", "request_id": "06304", "qc_table": "dna"}
 
-#     # save version in case of later edits that aren't compatible anymore
-#     submission = Submission(
-#         username=username,
-#         service_id=form_values['service_id'],
-#         transaction_id=None,
-#         material=form_values['material'],
-#         application=form_values['application'],
-#         form_values=json.dumps(form_values),
-#         grid_values=json.dumps(grid_values),
-#         version=VERSION,
-#     )
-        # db.session.add(submission)
-        # db.session.commit()
+# # save version in case of later edits that aren't compatible anymore
+# submission = Submission(
+#     username=username,
+#     service_id=form_values['service_id'],
+#     transaction_id=None,
+#     material=form_values['material'],
+#     application=form_values['application'],
+#     form_values=json.dumps(form_values),
+#     grid_values=json.dumps(grid_values),
+#     version=VERSION,
+# )
+# db.session.add(submission)
+# db.session.commit()
 
 #     responseObject = {
 #         'comments': comments,
 #     }
 
 #     return make_response(jsonify(responseObject), 200, None)
-
-
