1be932217c6f486b190ceebbb8e2358cc2d42525
Lisa Wagner <wagnerl@mskcc.org>
2019-08-23 09:32:59 -0400

    parameter prep

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # app/__init__.py
 
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 import json
 from flask_cors import CORS
@@ -25,9 +25,13 @@ db.create_all()
 #     return "Welcome to Sample QC Reporting"
 CORS(app)
 
-
 @app.route("/getComments")
 def comment():
+    # payload = request.get_json()['data']
+    # print(payload)
+    # form_values = payload['form_values']
+    # submissions = Submission.query.filter(Submission.username == username).all()
+
     comments = Comment.query.all()
     comments_response = []
     for comment in comments:
@@ -36,13 +40,19 @@ def comment():
     return json.dumps(comments_response)
 
 
-@app.route("/addComment")
+@app.route("/addComment", methods=['POST'])
 def save_comment():
     # some json
-    payload = '{            "username": "duniganm",            "user_title": "project manager",            "comment": "qc is ready",            "request_id": "06304_B",            "qc_table": "dna"      }'
+    payload = '{"username": "duniganm","user_title": "project manager","comment": "qc is ready","request_id": "06304_B","qc_table": "dna"}'
+
+    # payload = request.get_json()['data']
+    # print(payload)
+    # form_values = payload['form_values']
+    # submissions = Submission.query.filter(Submission.username == username).all()
 
     # parse payload
     pycomment = json.loads(payload)
+    # pycomment = payload
 
     # python dictionary is saved to db
     comment = Comment(
@@ -64,42 +74,7 @@ def save_comment():
         # columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
 
     return json.dumps(comments_response)
-    responseObject = {
-        'comments': comments,
-    }
+    responseObject = {'comments': comments}
 
     return make_response(jsonify(responseObject), 200, None)
 
-
-# /addComment
-# https://www.w3schools.com/python/python_json.asp
-# accepts a new comment like
-# {"username": "patrunoa", "user_title": "app analyst", "comment": "qc is ready", "request_id": "06304", "qc_table": "dna"}
-# insert/save it to the database
-# return all comments
-
-# @app.route('/saveSubmission', methods=['POST'])
-# def save_submission():
-
-# json - > python dictionary
-#     payload = {"username": "patrunoa", "user_title": "app analyst", "comment": "qc is ready", "request_id": "06304", "qc_table": "dna"}
-
-# # save version in case of later edits that aren't compatible anymore
-# submission = Submission(
-#     username=username,
-#     service_id=form_values['service_id'],
-#     transaction_id=None,
-#     material=form_values['material'],
-#     application=form_values['application'],
-#     form_values=json.dumps(form_values),
-#     grid_values=json.dumps(grid_values),
-#     version=VERSION,
-# )
-# db.session.add(submission)
-# db.session.commit()
-
-#     responseObject = {
-#         'comments': comments,
-#     }
-
-#     return make_response(jsonify(responseObject), 200, None)
