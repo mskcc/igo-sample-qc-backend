62d21f5d0af54e85436799436b6c6137c9731785
anna patruno <apatruno618@gmail.com>
2019-08-28 15:56:50 -0400

    /editComment and /deleteComment

--- app/views/comment.py
+++ app/views/comment.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, json, jsonify, request, make_response
 from app import db
 from app.models import Comment
+from sqlalchemy import update
 
 from datetime import datetime
 
@@ -12,7 +13,8 @@ comment = Blueprint('comment', __name__)
 @comment.route("/getComments", methods=['GET'])
 def get_comments():
 
-    comments_response = load_comments_for_request()
+    request_id = request.args.get("request_id")
+    comments_response = load_comments_for_request(request_id)
     # columnDefs.append(copy.deepcopy(possible_fields[column[0]]))
     responseObject = {'comments': comments_response}
     return make_response(jsonify(responseObject), 200, None)
@@ -47,6 +49,44 @@ def save_comment():
     return make_response(jsonify(responseObject), 200, None)
 
 
+# accepts comment_id, new content and updates the comment with new content
+# test using http://localhost:5000/editComment?id=3&content=%22new%20content%22
+@comment.route("/editComment", methods=['GET'])
+def edit_comment():
+    
+    # get the id of the comment
+    comment_id = request.args.get("id")
+    responseObject = {'id': comment_id}
+    
+    # get the new comment content
+    new_content = request.args.get("content")
+    
+    # update comment where the comment_id is equal to id in Comment table
+    comment = Comment.query.filter(
+        Comment.id == comment_id).first()
+    comment.comment = new_content
+    db.session.commit()
+
+    return make_response(jsonify(responseObject), 200, None)
+
+# accepts comment_id and deletes row
+@comment.route("/deleteComment", methods=['GET'])
+def delete_comment():
+
+    # get the id of the comment
+    comment_id = request.args.get("id")
+    responseObject = {'id': comment_id}
+    
+    # delete comment where the comment_id is equal to id in Comment table
+    comment = Comment.query.filter(
+        Comment.id == comment_id).first()
+    db.session.delete(comment)
+    db.session.commit()
+
+    return make_response(jsonify(responseObject), 200, None)
+
+
+
 # -------- UTIL --------
 
 
@@ -59,10 +99,10 @@ def load_comments():
     return comments_response
 
 
-# goes to Comment model/table and grabs comments for a specific request
-def load_comments_for_request():
+# goes to Comment model/table and grabs comments for a specific request_id
+def load_comments_for_request(request_id):
     comments = Comment.query.filter(
-        Comment.request_id == request.args.get("request_id"))
+        Comment.request_id == request_id)
     comments_response = []
     for x in comments:
         comments_response.append(x.serialize)
