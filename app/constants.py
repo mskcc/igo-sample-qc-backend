0df88f81c4f34bace09a250f5a02cbb395488615
Lisa Wagner <wagnerl@mskcc.org>
2019-09-13 16:09:34 -0400

    Building table data for FE, added constants.py

---
+++ app/constants.py
@@ -0,0 +1,138 @@
+sharedColumns = {
+    "sampleId": {
+        "limsField": "SampleId",
+        "field": "sampleId",
+        "columnHeader": "IGO ID",
+        "readOnly": True,
+    },
+    "otherSampleId": {
+        "limsField": "OtherSampleId",
+        "field": "otherSampleId",
+        "columnHeader": "Sample Name",
+        "readOnly": True,
+    },
+    # "altId": {"limsField": "AltId", "field": "altId", "columnHeader": "AltId","readOnly":True,},
+    "userSampleID": {
+        "limsField": "UserSampleID",
+        "field": "userSampleID",
+        "columnHeader": "UserSampleID",
+        "readOnly": True,
+    },
+    "concentrationUnits": {
+        "limsField": "ConcentrationUnits",
+        "field": "concentrationUnits",
+        "columnHeader": "ConcentrationUnits",
+        "readOnly": True,
+    },
+    "preservation": {
+        "limsField": "Preservation",
+        "field": "preservation",
+        "columnHeader": "Preservation",
+        "readOnly": True,
+    },
+    "recipe": {
+        "limsField": "Recipe",
+        "field": "recipe",
+        "columnHeader": "Recipe",
+        "readOnly": True,
+    },
+    "igoQcRecommendation": {
+        "limsField": "IgoQcRecommendation",
+        "field": "igoQcRecommendation",
+        "columnHeader": "IgoQcRecommendation",
+        "readOnly": True,
+    },
+    "comments": {
+        "limsField": "Comments",
+        "field": "comments",
+        "columnHeader": "Comments",
+        "readOnly": True,
+    },
+    "dateCreated": {
+        "limsField": "DateCreated",
+        "field": "dateCreated",
+        "columnHeader": "Date Created",
+        "readOnly": True,
+    },
+    "concentration": {
+        "limsField": "Concentration",
+        "field": "concentration",
+        "columnHeader": "Concentration",
+        "readOnly": True,
+    },
+    "volume": {
+        "limsField": "Volume",
+        "field": "volume",
+        "columnHeader": "Volume",
+        "readOnly": True,
+    },
+    "totalMass": {
+        "limsField": "TotalMass",
+        "field": "totalMass",
+        "columnHeader": "Total Mass",
+        "readOnly": True,
+    },
+}
+
+dnaColumns = {
+    "din": {
+        "limsField": "Din",
+        "field": "din",
+        "columnHeader": "Din",
+        "readOnly": True,
+    },
+    "percentHuman": {
+        "limsField": "PercentHuman",
+        "field": "percentHuman",
+        "columnHeader": "%Human",
+        "readOnly": True,
+    },
+    "tumorOrNormal": {
+        "limsField": "TumorOrNormal",
+        "field": "tumorOrNormal",
+        "columnHeader": "Tumor/Normal",
+        "readOnly": True,
+    },
+    "specimenType": {
+        "limsField": "SpecimenType",
+        "field": "specimenType",
+        "columnHeader": "CMO Sample Type",
+        "readOnly": True,
+    },
+}
+
+rnaColumns = {
+    "rin": {
+        "limsField": "Rin",
+        "field": "rin",
+        "columnHeader": "RIN",
+        "readOnly": True,
+    },
+    "rqn": {
+        "limsField": "Rqn",
+        "field": "rqn",
+        "columnHeader": "RQN",
+        "readOnly": True,
+    },
+    "dV200": {
+        "limsField": "DV200",
+        "field": "dV200",
+        "columnHeader": "DV200",
+        "readOnly": True,
+    },
+}
+
+libraryColumns = {
+    "avgSize": {
+        "limsField": "AvgSize",
+        "field": "avgSize",
+        "columnHeader": "Average Size",
+        "readOnly": True,
+    },
+    "tumorOrNormal": {
+        "limsField": "TumorOrNormal",
+        "field": "tumorOrNormal",
+        "columnHeader": "Tumor/Normal",
+        "readOnly": True,
+    },
+}
--- app/views/qc_report.py
+++ app/views/qc_report.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, json, jsonify, request, make_response
 import requests
-from app import app
+import copy
+from app import app, constants
 
 
 LIMS_API_ROOT = app.config["LIMS_API_ROOT"]
@@ -14,36 +15,45 @@ qc_report = Blueprint("qc_report", __name__)
 s = requests.Session()
 # s.mount("https://", MyAdapter())
 
-# three tables that will need to be connected in an endpoint? qcreportrna, dna, library
-# suggestion: controller - getqc_report(request_id), service - getqc_report
-@qc_report.route("/qc_report/rna")
-def qc_report_rna():
+
+# returns request level information including a list of samples in the request
+@qc_report.route("/getRequestSamples", methods=['GET'])
+def get_request_samples():
+    return_text = ""
+
+    request_id = request.args.get("request_id")
     # the API endpoint
     r = s.get(
-        # LIMS_API_ROOT + "/LimsRest/getProjectQc?project='09938'",
-        LIMS_API_ROOT + "/LimsRest/getPickListValues?list=Recipe",
+        LIMS_API_ROOT + "/api/getRequestSamples?request=" + request_id,
         auth=(LIMS_USER, LIMS_PW),
         verify=False,
     )
-    print(r.text)
-    # return "rna-result";
-    return r.text
+    print(r)
+    if r.status_code == 200:
+        return_text += r.text
+    else:
+
+        response = make_response(r.text, r.status_code, None)
+        return response
+
+    response = make_response(return_text, 200, None)
+    return response
 
 
 # add a new POST route /getQcReportSamples that
 @qc_report.route("/getQcReportSamples", methods=["POST"])
 def get_qc_report_samples():
     # makes an empty dictionary
-    data = dict()   
+    data = dict()
     # gets payload
-    payload = request.get_json()
+    payload = request.get_json()["data"]
     # parses payload into params
     request_id = payload["request"]
     samples = payload["samples"]
     # puts the params in the dictionary
     data['request'] = request_id
     data['samples'] = samples
-    
+
     # calls API endpoint and passes in necessary params
     r = s.post(
         LIMS_API_ROOT + "/getQcReportSamples",
@@ -51,5 +61,56 @@ def get_qc_report_samples():
         verify=False,
         data=data,
     )
-    # print(r)
-    return r.text
+    # print(constants.allColumns)
+    return_text = ""
+    if r.status_code == 200:
+        # assemble table data
+        lims_data = r.json()
+        tables = dict()
+        for field in lims_data:
+           
+            if field == "dnaReportSamples":
+                columnFeatures = Merge(constants.sharedColumns, constants.dnaColumns)
+                tables[field] = build_table(field, lims_data[field], columnFeatures)
+
+            if field == "rnaReportSamples":
+                columnFeatures = Merge(constants.sharedColumns, constants.rnaColumns)
+                tables[field] = build_table(field, lims_data[field], columnFeatures)
+            if field == "libraryReportSamples":
+                columnFeatures = Merge(
+                    constants.sharedColumns, constants.libraryColumns
+                )
+                tables[field] = build_table(field, lims_data[field], columnFeatures)
+        # print(tables)
+
+        return jsonify(tables)
+    else:
+  
+        response = make_response(r.text, r.status_code, None)
+        return response
+
+
+    response = make_response(return_text)
+    return response
+
+
+# -------------UTIL-------------
+# Python code to merge dict using a single
+# expression
+def Merge(dict1, dict2):
+    res = {**dict1, **dict2}
+    return res
+
+
+def build_table(reportTable, samples, columnFeatures):
+    responseColumns = dict()
+    responseSamples = copy.deepcopy(samples)
+    for column in samples[0]:
+        try:
+            responseColumns[column] = columnFeatures[column]
+        except:
+            print(column + " not found in expected columns for " + reportTable)
+            for sample in responseSamples:
+                sample.pop(column)
+
+    return {"data": responseSamples, "columnFeatures": responseColumns}
